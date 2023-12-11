5d3203f5e4f4cbb06cc3a4713d8be1716bf1b89c
Jamie Barrett <james.barrett@instapro.com>
2023-12-11 19:12:05 +0000

    chore: solution part 2 day 4

--- solutions/04.js
+++ solutions/04.js
@@ -10,4 +10,21 @@ const partOne = data.reduce((prev, [winning, yours]) => {
   return prev + winning.reduce((prev, curr) => (yours.includes(curr) ? prev * 2 || 1 : prev), 0);
 }, 0);
 
-console.log(partOne);
+const partTwo = () => {
+  const cardCounter = {};
+
+  data
+    .map(([winning, yours], idx) => {
+      cardCounter[idx] = 1;
+      return winning.reduce((prev, curr) => (yours.includes(curr) ? (prev += 1) : prev), 0);
+    })
+    .forEach((copiesWon, idx) => {
+      for (let index = 0; index < copiesWon; index++) {
+        cardCounter[idx + 1 + index] += cardCounter[idx];
+      }
+    });
+
+  return Object.values(cardCounter).reduce((prev, curr) => prev + curr, 0);
+};
+
+console.log(partTwo());
